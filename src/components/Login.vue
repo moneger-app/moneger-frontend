9c8cdebd092a2b06cb8da65a049f933f0eb10c93
nikita-volgin <113235937+nikita-volgin@users.noreply.github.com>
2023-02-14 14:15:20 +0600

    google auth button corrected

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="wrap">
+    <div class="wrap text-center">
         <h2>Добрый день</h2>
         <p>Просьба авторизоваться через Ваш Google-аккаунт</p>
-        <div
-            id="g_id_onload"
-            data-client_id="946108256311-vruaonqmq8pb44amtrk99cvcj3e27ir5.apps.googleusercontent.com"
-            data-callback="googleSignIn"
-            data-auto_prompt="false"
-        >
+        <div class="google-auth-button mx-auto" style="max-width: 200px; overflow: hidden">
+            <div
+                id="g_id_onload"
+                data-client_id="946108256311-vruaonqmq8pb44amtrk99cvcj3e27ir5.apps.googleusercontent.com"
+                data-callback="googleSignIn"
+                data-auto_prompt="false"
+            >
+            </div>
+            <div class="g_id_signin" data-type="standard"></div>
         </div>
-        <div class="g_id_signin" data-type="standard"></div>
     </div>
 </template>
 
