c77f35689be9314dc07512c704cd75b370664bcc
nikita-volgin <113235937+nikita-volgin@users.noreply.github.com>
2023-02-14 16:16:13 +0600

    google button auth corrected

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
     <div class="wrap text-center">
         <h2>Добрый день</h2>
         <p>Просьба авторизоваться через Ваш Google-аккаунт</p>
-        <div class="google-auth-button mx-auto" style="max-width: 200px; overflow: hidden">
+        <div class="google-auth-button mx-auto overflow-hidden">
             <div
                 id="g_id_onload"
                 data-client_id="946108256311-vruaonqmq8pb44amtrk99cvcj3e27ir5.apps.googleusercontent.com"
@@ -10,7 +10,13 @@
                 data-auto_prompt="false"
             >
             </div>
-            <div class="g_id_signin" data-type="standard"></div>
+            <div
+                class="g_id_signin"
+                data-type="standard"
+                :data-theme="$vuetify.theme.dark ? 'filled_black' : 'outline'"
+                data-text="continue_with"
+                data-width=250
+            />
         </div>
     </div>
 </template>
@@ -37,7 +43,15 @@ export default {
 </script>
 
 <style scoped>
+@media screen and (min-width: 400px) {
+    .wrap {
+        font-size: 2rem;
+    }
+}
 .wrap {
     margin: 0 auto;
+}
+.google-auth-button {
+    max-width: 250px;
 }
 </style>
